fa71e092c72c3a0a6355e775a709a6da51fc9160
zhilin010 <zhilin010@gmail.com>
2023-02-26 12:39:35 -0500

    Add basic animation template for landing page

---
+++ app.js
@@ -0,0 +1,30 @@
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        };
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden-translate');
+hiddenElements.forEach((el) => observer.observe(el));
+
+// animate "typing" animation 
+const text = document.querySelector(".self-presentation")
+
+const textLoad = () => {
+    setTimeout(() => {
+        text.textContent = "My name is Zhi Lin, nice to meet you."
+    }, 2000);
+    setTimeout(() => {
+        text.textContent = "I like to make extraordinary websites."
+    }, 8000);
+    setTimeout(() => {
+        text.textContent = "Now, scroll down and enjoy your stay."
+    }, 14000);
+}
+
+textLoad();
